fe6ccb9ac26e48c13373a5564443838589253852
Zestgeek-26 <zestgeek@Zestgeek-26s-MacBook-Air.local>
2021-09-01 16:54:22 +0530

    worked on deshbord pages

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -3,21 +3,15 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-
-				<!-- <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre> Language
-				<span class="caret"></span>
-				</a>
-				<div class="dropdown-menu">
-				<a class="dropdown-item" href="/lang/en">English</a>
-				<a class="dropdown-item" href="/lang/ar">Arabic</a>
-				</div> -->
-
-                <div class="card-header">{{ __('language.dashboard') }}</div>
-
-                
-            </div>
+        <div class="col-md-12">
+        	<div class="deshbord-sec">
+	            <div class="card deshbord-first-login mb-4">
+	            	<img src="/images/site-images/SANAD Logo.gif" class="img-fluid">
+	            	<h4>Welcome to your new place </h4>
+	            	<p>Lets create your first orgnization </p>
+	            	<a href="/organisation/create" class="btn">Create New Orgnization</a>
+	            </div>
+   			</div>
         </div>
     </div>
 </div>
